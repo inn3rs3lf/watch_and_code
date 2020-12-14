921b019100c5354be9e6462ef6c759a0fb3ec634
Darryl Le Roux <le.roux.darryl@gmail.com>
2020-12-14 22:30:51 +0200

    Refactored the code to include functions

--- app.js
+++ app.js
@@ -2,11 +2,46 @@
 ///// Version One /////
 ///////////////////////
 
-let todos = ['Item 1', 'Item 2', 'Item 3']; //Stores the todo list
+let todos = ["Item 1", "Item 2", "Item 3"]; // Stores the todo list
 
-todos.push('Item 4'); //Adds a todo - .push adds something to the end of an array
+//todos.push("Item 4"); // Adds a todo - .push adds something to the end of an array
 
-todos[0] = 'Item 1 updated' // Edits a todo 
+console.log(todos);
 
-todos.splice(0, 1); // Removes a todo. Splice argument 0 is the index, and 1 is the amount of items to delete
+// function to add a todo 
+function add(newData) {
+  todos.push(newData);
+  console.log(todos);
+}
+
+//function to edit the todo
+function edit(index, newValue) {
+  todos[index] = newValue
+  todos[index] = newValue
+  console.log(todos);
+}
+
+//function to remove a todo
+function remove(position) {
+  todos.splice(position, 1);
+  console.log(todos);
+}
+
+
 console.log(todos); // Displays the todo
+
+//// Functions ////
+// Functions groupe multiple lines of code together under a single name
+// When you run a function, you can provide the function with data
+
+// function demoFunction(myData) { // myData acts as a variable (parameter)
+//   console.log(myData);
+// }
+
+// demoFunction('Darryl'); // myData = 'Darryl' (argument)
+
+/////////////////////
+///// Version 2 /////
+/////////////////////
+
+
